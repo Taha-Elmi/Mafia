a467802bedbd976c6a17f728a3196fa4e5aead92
TahaElmi <elmi.taha@gmail.com>
2021-06-07 09:34:06 +0430

    feat: the structure of the roles is added

---
+++ src/server/Role.java
@@ -0,0 +1,38 @@
+package server;
+
+public class Role {
+    public interface Mafia {
+
+    }
+    public class GodFather extends Role implements Mafia{
+
+    }
+    public class SimpleMafia extends Role implements Mafia{
+
+    }
+    public class DrLecter extends Role implements Mafia{
+
+    }
+
+    public class Doctor extends Role {
+
+    }
+    public class Detective extends Role {
+
+    }
+    public class Professional extends Role {
+
+    }
+    public class SimpleCitizen extends Role {
+
+    }
+    public class Mayor extends Role {
+
+    }
+    public class Psychiatrist extends Role {
+
+    }
+    public class DieHard extends Role {
+
+    }
+}
